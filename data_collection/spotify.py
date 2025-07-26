69054cd48626c9c2c4962f2d2d9d827c49ab54a9
Tomas Gutierrez <gutierreztomas321@gmail.com>
2025-07-26 18:53:56 -0400

    spotify api, lastfm api, genius api

---
+++ data_collection/spotify.py
@@ -0,0 +1,28 @@
+#%%
+
+import os
+from requests import get, post
+from pprint import pprint
+
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+res = post(
+    "https://accounts.spotify.com/api/token",
+    headers={"Content-Type": "application/x-www-form-urlencoded"},
+    data={"grant_type": "client_credentials"},
+    auth=(client_id, client_secret),
+)
+
+# %%
+
+access_token = res.json()["access_token"]
+headers = {
+    "Authorization": f"Bearer {access_token}"
+}
+
+# %%
+
+res = get("https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/related-artists", headers=headers)
+pprint(res.json())
+# %%
