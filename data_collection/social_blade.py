015d48a838d2c3098b47c2d95a387afc5da68da1
Tomas Gutierrez <gutierreztomas321@gmail.com>
2025-08-31 18:53:42 -0400

    IG and TT vault data

--- data_collection/social_blade.py
+++ data_collection/social_blade.py
@@ -4,8 +4,12 @@ import requests
 import os
 from pprint import pprint
 
+#%%
+
+# The Below can be used for Instagram Vault API Calls
+
 base_url = "https://matrix.sbapis.com/b"
-endpoint = "/instagram/statistics?query=SocialBlade&history=archive&allow-stale=false"
+endpoint = "/instagram/statistics?query=SocialBlade&history=vault&allow-stale=false"
 
 
 headers = {
@@ -20,4 +24,22 @@ data = response.json()
 
 df = pd.DataFrame(data["data"]["daily"])
 df
+# %%
+
+# The Below can be used for TikTok Vault API Calls
+
+base_url = "https://matrix.sbapis.com/b"
+endpoint = "/tiktok/statistics?query=SocialBlade&history=vault&allow-stale=false"
+
+
+headers = {
+    "clientid": os.getenv("SOCIAL_BLADE_CLIENT_ID"),
+    "token": os.getenv("SOCIAL_BLADE_API_TOKEN")
+}
+
+response = requests.get(base_url + endpoint, headers=headers)
+
+data = response.json()
+# %%
+pprint(data)
 # %%
